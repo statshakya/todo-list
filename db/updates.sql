d4500f4433f76c0af54a7a59ef71d76790b226cf
Swarna-Shakya <swarnamanshakya@gmail.com>
2025-07-27 12:41:09 +0545

    [ADD] DB changes

--- db/updates.sql
+++ db/updates.sql
@@ -1,47 +1,25 @@
 -- SMS 2025-07-24
 ALTER TABLE `tbl_tododata` ADD `type` VARCHAR(50) NOT NULL AFTER `title`; 
 
--- sahas 2025-07-26
--- Adminer 4.8.1 MySQL 9.2.0 dump
-
-SET NAMES utf8;
-SET time_zone = '+00:00';
-SET foreign_key_checks = 0;
-SET sql_mode = 'NO_AUTO_VALUE_ON_ZERO';
-
-SET NAMES utf8mb4;
-
-DROP TABLE IF EXISTS `tbl_notes`;
-CREATE TABLE `tbl_notes` (
-  `id` int NOT NULL AUTO_INCREMENT,
-  `title` varchar(255) NOT NULL,
-  `content` text NOT NULL,
-  `user_id` int NOT NULL,
-  `fileupload` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci DEFAULT NULL,
-  `created_date` timestamp NOT NULL,
-  `Updated_date` timestamp NOT NULL,
-  PRIMARY KEY (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
-
-INSERT INTO `tbl_notes` (`id`, `title`, `content`, `user_id`, `fileupload`, `created_date`, `Updated_date`) VALUES
-(7,	'da',	'sdasdasd',	1,	NULL,	'2025-07-26 07:49:59',	'2025-07-26 08:16:08');
-
--- 2025-07-26 08:18:23
-
-SET NAMES utf8;
-SET time_zone = '+00:00';
-SET foreign_key_checks = 0;
-SET sql_mode = 'NO_AUTO_VALUE_ON_ZERO';
-
-SET NAMES utf8mb4;
-
-DROP TABLE IF EXISTS `tbl_events`;
+-- SMS 2025-07-27
 CREATE TABLE `tbl_events` (
-  `id` int NOT NULL AUTO_INCREMENT,
+  `id` int(11) NOT NULL,
   `title` varchar(255) NOT NULL,
   `event_date` date NOT NULL,
-  `user_id` int NOT NULL,
-  `created_date` timestamp NOT NULL ON UPDATE CURRENT_TIMESTAMP,
-  `updated_date` timestamp NOT NULL ON UPDATE CURRENT_TIMESTAMP,
-  PRIMARY KEY (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+  `user_id` int(11) NOT NULL,
+  `created_date` datetime NOT NULL,
+  `updated_date` datetime NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+CREATE TABLE `tbl_notes` (
+  `id` int(11) NOT NULL,
+  `title` varchar(255) NOT NULL,
+  `content` text NOT NULL,
+  `user_id` int(11) NOT NULL,
+  `fileupload` text NOT NULL,
+  `created_date` datetime NOT NULL,
+  `updated_date` datetime NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+ALTER TABLE `tbl_events` MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+ALTER TABLE `tbl_notes` MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
